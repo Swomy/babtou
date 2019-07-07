45eaa424919233fdbfe1643c36cb47f0a2246531
Swomy <52625672+Swomy@users.noreply.github.com>
2019-07-07 15:59:52 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "p";
 
-client.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
 
 client.on('message', message => {
     if(message.content === "phelp"){
